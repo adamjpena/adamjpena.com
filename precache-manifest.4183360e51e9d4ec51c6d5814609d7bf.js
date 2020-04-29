1fd0e5a2a64caab94a0d2314ebddaa36cf167cff
Adam Peña <adamjpena@gmail.com>
2020-04-29 16:38:29 -0700

    Updates

---
+++ precache-manifest.4183360e51e9d4ec51c6d5814609d7bf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b4b632d6213ed3a8552e1e9c438ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9464b26fbd6a86dfb50c",
+    "url": "/static/css/2.342be520.chunk.css"
+  },
+  {
+    "revision": "543183b48cbbc0aa7900",
+    "url": "/static/css/main.f1d96dc6.chunk.css"
+  },
+  {
+    "revision": "9464b26fbd6a86dfb50c",
+    "url": "/static/js/2.993a4572.chunk.js"
+  },
+  {
+    "revision": "543183b48cbbc0aa7900",
+    "url": "/static/js/main.fb9d9f2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0612a06fc613a637c9a47b1d481450f0",
+    "url": "/static/media/adam-city-vertical.0612a06f.jpg"
+  },
+  {
+    "revision": "a59578d1cd8983ef8b685a755486c1dc",
+    "url": "/static/media/adam-city-vertical@2x.a59578d1.jpg"
+  },
+  {
+    "revision": "b43bd8dbce6063b1c849b2419bfd78b9",
+    "url": "/static/media/adam-city.b43bd8db.jpg"
+  },
+  {
+    "revision": "78ea9620079bb3c35f6cee0c17f040d9",
+    "url": "/static/media/adam-city@2x.78ea9620.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "ba3bc198bf778f836588926645146ce1",
+    "url": "/static/media/nebula-background.ba3bc198.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.0888448e203211ae5f45acfeafe3be3c.js"
+  "/precache-manifest.4183360e51e9d4ec51c6d5814609d7bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

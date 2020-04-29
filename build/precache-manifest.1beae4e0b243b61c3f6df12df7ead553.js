7be60f4e0b0e3c23df6e5aa93d9fe506e5bf11fb
Adam Peña <adamjpena@gmail.com>
2020-04-28 19:24:39 -0700

    remove build from gitignore

---
+++ build/precache-manifest.1beae4e0b243b61c3f6df12df7ead553.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bd20d5842a4bc88d1884bce21cf58f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a9304445c013ee306f2",
+    "url": "/static/css/2.cbff686f.chunk.css"
+  },
+  {
+    "revision": "99440347127606034b7b",
+    "url": "/static/css/main.7fc57668.chunk.css"
+  },
+  {
+    "revision": "6a9304445c013ee306f2",
+    "url": "/static/js/2.4d73a516.chunk.js"
+  },
+  {
+    "revision": "99440347127606034b7b",
+    "url": "/static/js/main.e34333c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0612a06fc613a637c9a47b1d481450f0",
+    "url": "/static/media/adam-city-vertical.0612a06f.jpg"
+  },
+  {
+    "revision": "a59578d1cd8983ef8b685a755486c1dc",
+    "url": "/static/media/adam-city-vertical@2x.a59578d1.jpg"
+  },
+  {
+    "revision": "b43bd8dbce6063b1c849b2419bfd78b9",
+    "url": "/static/media/adam-city.b43bd8db.jpg"
+  },
+  {
+    "revision": "78ea9620079bb3c35f6cee0c17f040d9",
+    "url": "/static/media/adam-city@2x.78ea9620.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "ba3bc198bf778f836588926645146ce1",
+    "url": "/static/media/nebula-background.ba3bc198.jpg"
+  }
+]);

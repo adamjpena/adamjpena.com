2fdf2e00005ae59f29ff908a956012a671f1639f
Adam Peña <adamjpena@gmail.com>
2020-04-30 00:58:50 -0700

    add resume data file

---
+++ src/store/resume.js
@@ -0,0 +1,146 @@
+export const resumeData = {
+  firstName: "Adam",
+  lastName: "Pena",
+  role: "Software Engineer",
+  objectiveSection: {
+    title: "Objective",
+    content:
+      "Developer with a keen eye for design and a passion for innovation and experimentation to enhance the user experience"
+  },
+  skillsSection: {
+    title: "Skills",
+    sections: [
+      {
+        title: "Programming",
+        bullets: [
+          "Javascript",
+          "ReactJS / Native",
+          "TypeScript",
+          "HTML5",
+          "CSS3",
+          "Ruby on Rails",
+          "Node.js",
+          "LESS",
+          "Sass",
+          "Redux",
+          "jQuery",
+          "Jest",
+          "RSpec",
+          "Webpack",
+          "Docker",
+          "Bootstrap",
+          "Shell"
+        ]
+      },
+      {
+        tools: "Tools",
+        bullets: [
+          "Git/GitHub",
+          "Docker",
+          "Pivotal",
+          "JIRA",
+          "Zeplin",
+          "Photoshop",
+          "Illustrator"
+        ]
+      }
+    ]
+  },
+  educationSection: {
+    title: "Education",
+    schools: [
+      {
+        name: "University of Illinois",
+        degree: "B.S. Community Health",
+        graduation: "May 2014",
+        location: "Champaign, IL",
+        accolades: ["Dean's List"]
+      }
+    ]
+  },
+  linksSection: {
+    title: "Links",
+    links: [
+      {
+        text: "GitHub:// adamjpena",
+        url: "https://www.github.com/adamjpena"
+      },
+      {
+        text: "LinkedIn:// adamjpena",
+        url: "https://www.linkedin.com/in/adamjpena"
+      },
+      {
+        text: "adamjpena.com",
+        url: "https://adamjpena.com"
+      }
+    ]
+  },
+  experienceSection: {
+    title: "Experience",
+    positions: [
+      {
+        company: "Visibly",
+        companyDescription: "(formerly Opternative)",
+        roles: [
+          {
+            title: "Software Engineer",
+            start: "Sep 2017",
+            end: "Jul 2019",
+            location: "Chicago, IL",
+            bullets: [
+              "Optimize user onboarding and offboarding to increase conversion rate by alleviating friction in the user experience",
+              "Architect, prototype, build, test, and deploy new interface components and functionality for online eye exam and vision care platform",
+              "Mentor junior level developers on software development process",
+              "Monitor technical support channels to document bugs and implement solutions with test coverage"
+            ],
+            techStack:
+              "HTML5/CSS3, ReactJS, Angular, JavaScript, Ruby on Rails, Sass"
+          }
+        ]
+      },
+      {
+        company: "Arrive",
+        companyDescription: "(formerly ParkWhiz)",
+        roles: [
+          {
+            title: "Front End Developer",
+            start: "Mar 2017",
+            end: "Aug 2017",
+            location: "Chicago, IL"
+          },
+          {
+            title: "Jr. Front End Developer",
+            start: "Mar 2016",
+            end: "Mar 2017",
+            location: "Chicago, IL",
+            bullets: [
+              "Overhauled parking widget to leverage new API endpoints to acutely reduce load time while extending functionality and adaptability to partner specs",
+              "Designed and developed responsive site-wide refreshes, landing pages and email templates",
+              "Identified A/B test opportunities for site improvements",
+              "Investigated and fixed reported issues and wrote tests to prevent regression"
+            ],
+            techStack:
+              "HTML5/CSS3, ReactJS, JavaScript, Ruby on Rails, Haml, Sass, PHP"
+          }
+        ]
+      }
+    ]
+  },
+  projectsSection: {
+    title: "Code",
+    projects: [
+      {
+        title: "Opternative/Visibly brand refresh, and optimization",
+        url: "https://www.github.com/adamjpena",
+        content:
+          "Worked with designers to develop and implement a brand refresh and name change for our high volume, consumer facing site. Revamped user experience with new streamlined interactions. Improved load speed by advocating for and adopting reusable design practice while refactoring and removing legacy stylesheets."
+      },
+      {
+        title: "LinkedIn:// adamjpena",
+        url: "https://www.linkedin.com/in/adamjpena",
+        content:
+          "Designed and developed a business platform subdomain for businesses to find parking, book and manage employee and visitor parking. Elevated a basic interface to a modernized, cross-browser compatible and responsive portal for over 40% of ParkWhiz’s repeat customers driving significant savings for partners."
+      }
+    ]
+  }
+};

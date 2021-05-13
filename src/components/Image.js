d3c688839571386f9f24991453b52b205ea32804
Adam Peña <adamjpena@gmail.com>
2021-05-12 23:35:02 -0500

    stars optimization

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import cx from "classnames";
 import styles from "./Image.module.scss";
 
 const Image = ({ classes, ...restRrops }) => (
-  <img className={cx(classes, styles.img)} {...restRrops} />
+  <img className={cx(classes, styles.img)} alt="" {...restRrops} />
 );
 
 export default Image;

f47bc113d2b44d292fbe8fe3646c6e2afc6f4df7
Adam Peña <adamjpena@gmail.com>
2021-05-19 01:54:14 -0500

    Updates

---
+++ component---src-pages-404-tsx-3d2670b119fbe844c1c8.js
@@ -0,0 +1,2 @@
+(self.webpackChunkadamjpena_com=self.webpackChunkadamjpena_com||[]).push([[218],{5947:function(e,n,a){"use strict";a.r(n);var t=a(7294),l=a(1596),u=a(2279),c=a(8017);n.default=function(){return t.createElement(l.zw,null,t.createElement(u.W2,null,t.createElement(c.X6,null,"404 not found")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-3d2670b119fbe844c1c8.js.map
